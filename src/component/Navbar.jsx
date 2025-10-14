fd7298385930f644a2a78ef68843752824c654c3
Shubham Thakre <shubhamthakre43@gmail.com>
2025-10-14 15:27:22 +0530

    added nav list

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,17 +1,51 @@
 import React from 'react'
-import {logo} from '../../public/Images/image 1.png'
+import{PiMagnifyingGlassBold} from 'react-icons/pi'
 
 const Navbar = () => {
-  return (
-    <div className='bg-white'>
-      <div className="left-section">
-        <img src={logo} alt="" />
-      </div>
-      <div className="left-section">
+    return (
+        <div className='bg-white p-4 flex justify-between items-center'>
+            <div className="flex items-center gap-5">
+                <img src="\public\Images\image 1.png" className='w-50' alt="" />
+                <ul className='md:text-lg font-semibold flex gap-5'>
+                    <li>
+                        <button
+                            className='py-2 px-4 rounded-md hover:bg-gray-300'
+                        >
+                            Start
+                        </button>
+                    </li>
+                    <li>
+                        <button
+                            className='py-2 px-4 rounded-md hover:bg-gray-300'
+                        >
+                            Use
+                        </button>
+                    </li>
+                    <li>
+                        <button
+                            className='py-2 px-4 rounded-md hover:bg-gray-300'
+                        >
+                            Develop
+                        </button>
+                    </li>
+                </ul>
+            </div>
+            {/* <div className="flex items-center gap-4">
+                <div className="flex items-center border border-gray-300 rounded-full px-4 py-2">
+                    <input
+                        type="text"
+                        placeholder="SEARCH"
+                        className="bg-transparent text-sm font-semibold outline-none placeholder-black uppercase"
+                    />
+                    <magnifying className="w-4 h-4 ml-2 text-black" />
+                </div>
 
-      </div>
-    </div>
-  )
+                <button className="bg-black text-white font-semibold rounded-full px-6 py-2 text-sm uppercase hover:bg-gray-800 transition">
+                    Connect Wallet
+                </button>
+            </div> */}
+        </div>
+    )
 }
 
 export default Navbar
--- src/routes/AppRoute.jsx
+++ src/routes/AppRoute.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Navbar from '../component/Navbar'
 
 const AppRoute = () => {
   return (
     <div>
-      
+      <Routes>
+        <Route />
+      </Routes>
     </div>
   )
 }
