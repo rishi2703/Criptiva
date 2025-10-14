16d249dc107576b80b01a717c81e4e2380b2aa2e
Shubham Thakre <shubhamthakre43@gmail.com>
2025-10-14 14:51:29 +0530

    added navbar and images

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import {logo} from '../../public/Images/image 1.png'
 
 const Navbar = () => {
   return (
-    <div>
-      
+    <div className='bg-white'>
+      <div className="left-section">
+        <img src={logo} alt="" />
+      </div>
+      <div className="left-section">
+
+      </div>
     </div>
   )
 }
