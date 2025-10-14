8a0a35a412ab9461ecbab518d20b434985f6fb07
Rishi Vijay Vargiya <harsh.vijayvargiya@aplitetech.com>
2025-10-14 18:41:48 +0530

    added Dashboard Layout and MainLayout

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+
 import './App.css'
 import Navbar from './component/Navbar'
 import AppRoute from './routes/AppRoute'
@@ -8,9 +8,14 @@ function App() {
     <div>
       <div className='fixed top-0 left-0 bg-transparent w-full p-4'>
         <Navbar />
+
       </div>
       <AppRoute />
+      <div>
+    
     </div>
+    </div>
+    
   )
 }
 
